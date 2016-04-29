50dde0c02f93714804cea9d508fcb920e4887266
premkumaranand <aa.premkumar@gmail.com>
2016-04-29 10:26:11 +0100

    Init

---
+++ MasterNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekhub.Extensions;
+
+namespace Tekhub.Umbraco.NodeMapper.Core
+{
+    public class MasterNode
+    {
+        public MasterNode() { }
+
+        public MasterNode(MasterNode toCopy)
+        {
+            Id = toCopy.Id;
+            Name = toCopy.Name;
+            Url = toCopy.Url;
+            PageTitle = toCopy.PageTitle;
+            DescriptionMeta = toCopy.DescriptionMeta;
+            TitleMeta = toCopy.TitleMeta;
+            KeyWordsMeta = toCopy.KeyWordsMeta;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public string PageTitle { get; set; }
+        public string DescriptionMeta { get; set; }
+        public string TitleMeta { get; set; }
+        public string KeyWordsMeta { get; set; }
+        public DateTime PublishedDate { get; set; }
+
+        public string UrlLastSegment
+        {
+            get { return Url.GetLastUrlSegment(); }
+        }
+
+        public virtual void SetProperties(Dictionary<string, object> propertyValueMappings)
+        {
+            PageTitle = Convert.ToString(propertyValueMappings["pageTitle"]);
+            DescriptionMeta = Convert.ToString(propertyValueMappings["descriptionMeta"]);
+            TitleMeta = Convert.ToString(propertyValueMappings["titleMeta"]);
+            KeyWordsMeta = Convert.ToString(propertyValueMappings["keyWordsMeta"]);
+        }
+    }
+}
